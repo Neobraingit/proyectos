61fde64b7d81852223e90378d188a6c433767142
Neobraingit <Maervca2@icloud.com>
2023-05-25 20:09:47 +0200

    Reproductor

---
+++ Reproductor_música/Funciones.py
@@ -0,0 +1,10 @@
+from playsound import playsound 
+
+def reproductor():
+    archivo1 = 'alone.mp3'
+    playsound(archivo1)
+    
+def reproductor2():
+    archivo2= 'snake.mp3'
+    playsound(archivo2)
+    
---
+++ Reproductor_música/desarrollo.py
@@ -0,0 +1,10 @@
+import Funciones
+
+print ('--------Reproductor--------')
+
+print ('Elige una opción: \na \nb')
+eleccion = input()
+if eleccion == 'a':
+    Funciones.reproductor()
+else:
+    Funciones.reproductor2()
