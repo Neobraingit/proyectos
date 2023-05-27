ea90a55601a7d83d1cbb55eccf17af20833aa8f1
Neobraingit <Maervca2@icloud.com>
2023-05-27 16:52:58 +0200

    Reproductor

--- Reproductor_música/desarrollo.py
+++ Reproductor_música/desarrollo.py
@@ -1,7 +1,17 @@
 import Funciones
 
-print ('--------Reproductor--------')
-
+print ('''  o__ __o                                                              o                            o                             
+ <|     v\                                                            <|>                          <|>                            
+ / \     <\                                                           < \                          < >                            
+ \o/     o/      o__  __o   \o_ __o    \o__ __o     o__ __o      o__ __o/   o       o       __o__   |        o__ __o    \o__ __o  
+  |__  _<|      /v      |>   |    v\    |     |>   /v     v\    /v     |   <|>     <|>     />  \    o__/_   /v     v\    |     |> 
+  |       \    />      //   / \    <\  / \   < >  />       <\  />     / \  < >     < >   o/         |      />       <\  / \   < > 
+ <o>       \o  \o    o/     \o/     /  \o/        \         /  \      \o/   |       |   <|          |      \         /  \o/       
+  |         v\  v\  /v __o   |     o    |          o       o    o      |    o       o    \\         o       o       o    |        
+ / \         <\  <\/> __/>  / \ __/>   / \         <\__ __/>    <\__  / \   <\__ __/>     _\o__</   <\__    <\__ __/>   / \       
+                            \o/                                                                                                   
+                             |                                                                                                    
+                            / \                                                                                                   ''')
 print ('Elige una opción: \na \nb')
 eleccion = input()
 if eleccion == 'a':
